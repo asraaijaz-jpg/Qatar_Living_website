018d16d4ea4b5892917d0dde4c90c86b860251bb
asraaijaz-jpg <63543965+asraaijaz-jpg@users.noreply.github.com>
2023-03-20 21:41:01 +0500

    Updates

---
+++ precache-manifest.4356b0a2a7756ca21fd931f5e1251b11.js
@@ -0,0 +1,410 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebfc4bec0e5ed33a8a41",
+    "url": "/Qatar_Living_website/static/css/main.65f9acb3.chunk.css"
+  },
+  {
+    "revision": "ebfc4bec0e5ed33a8a41",
+    "url": "/Qatar_Living_website/static/js/main.033738be.chunk.js"
+  },
+  {
+    "revision": "97a50df94ae60b15b3a5",
+    "url": "/Qatar_Living_website/static/js/runtime~main.bd0f858a.js"
+  },
+  {
+    "revision": "9adcac7b487f9b7ae108",
+    "url": "/Qatar_Living_website/static/css/2.84d9890b.chunk.css"
+  },
+  {
+    "revision": "9adcac7b487f9b7ae108",
+    "url": "/Qatar_Living_website/static/js/2.8f548168.chunk.js"
+  },
+  {
+    "revision": "235b968a5a9ca1eceaf5",
+    "url": "/Qatar_Living_website/static/js/3.8928424f.chunk.js"
+  },
+  {
+    "revision": "013f4b1e5f4573822866d69a36ff4509",
+    "url": "/Qatar_Living_website/static/media/b1.013f4b1e.PNG"
+  },
+  {
+    "revision": "8bbe0588ef184489145260e0d24352ed",
+    "url": "/Qatar_Living_website/static/media/b2.8bbe0588.PNG"
+  },
+  {
+    "revision": "16c80d1abb440129067d8c11498d45e1",
+    "url": "/Qatar_Living_website/static/media/qatar.16c80d1a.PNG"
+  },
+  {
+    "revision": "7893c2856f22bea0bfe24b26fb3043b3",
+    "url": "/Qatar_Living_website/static/media/img1.7893c285.jpg"
+  },
+  {
+    "revision": "bc0a41cdec9087bcca099e723f7fc6b0",
+    "url": "/Qatar_Living_website/static/media/img2.bc0a41cd.jpg"
+  },
+  {
+    "revision": "455288c333a88d9eef6e0615bc6df431",
+    "url": "/Qatar_Living_website/static/media/img3.455288c3.jpg"
+  },
+  {
+    "revision": "f57bd6317445f407a9f8998f5fbd619f",
+    "url": "/Qatar_Living_website/static/media/img5.f57bd631.png"
+  },
+  {
+    "revision": "d9781b85dd585096602d25743f53d751",
+    "url": "/Qatar_Living_website/static/media/img6.d9781b85.jpeg"
+  },
+  {
+    "revision": "d1b94128aa6c24c3385d203d0da3c69c",
+    "url": "/Qatar_Living_website/static/media/img7.d1b94128.jpg"
+  },
+  {
+    "revision": "17b38e38caac59137445e10cf2ac3d4d",
+    "url": "/Qatar_Living_website/static/media/img8.17b38e38.jpg"
+  },
+  {
+    "revision": "0e353e8ca6264b5facf3b568120440c8",
+    "url": "/Qatar_Living_website/static/media/img9.0e353e8c.jpg"
+  },
+  {
+    "revision": "ac1c20fc7c15d40b9c90c1d7ff086c09",
+    "url": "/Qatar_Living_website/static/media/img10.ac1c20fc.jpg"
+  },
+  {
+    "revision": "f9dc14e10a7ec094bd83255c2c6fcf41",
+    "url": "/Qatar_Living_website/static/media/img11.f9dc14e1.jpg"
+  },
+  {
+    "revision": "dc93a3fc602f57ff95be46b081e4a386",
+    "url": "/Qatar_Living_website/static/media/img12.dc93a3fc.png"
+  },
+  {
+    "revision": "487de7bed405044e57282bb281f39867",
+    "url": "/Qatar_Living_website/static/media/img13.487de7be.jpg"
+  },
+  {
+    "revision": "de5d7ca9e8449a0eb06f3043ad1cecd8",
+    "url": "/Qatar_Living_website/static/media/img14.de5d7ca9.png"
+  },
+  {
+    "revision": "1bae81517598aa9f6750776dbf63b187",
+    "url": "/Qatar_Living_website/static/media/img15.1bae8151.png"
+  },
+  {
+    "revision": "796c28dccccb15f4060bda7037b171e9",
+    "url": "/Qatar_Living_website/static/media/img16.796c28dc.png"
+  },
+  {
+    "revision": "6828d8bb2f7fcc3f14e6e20f6a566e16",
+    "url": "/Qatar_Living_website/static/media/img17.6828d8bb.jpg"
+  },
+  {
+    "revision": "dc3bcb7fd826046edf01c8d92bc06de0",
+    "url": "/Qatar_Living_website/static/media/img18.dc3bcb7f.jpg"
+  },
+  {
+    "revision": "d8adaa46ea31e8a1970b75693263b1d3",
+    "url": "/Qatar_Living_website/static/media/img19.d8adaa46.jpg"
+  },
+  {
+    "revision": "85736a7bea7273a3edd1eebc28133abf",
+    "url": "/Qatar_Living_website/static/media/img20.85736a7b.png"
+  },
+  {
+    "revision": "0623f2e3324fbae6619faa1a074b347e",
+    "url": "/Qatar_Living_website/static/media/img21.0623f2e3.jpg"
+  },
+  {
+    "revision": "460a5c884b5d42986f658bfd6fe2c097",
+    "url": "/Qatar_Living_website/static/media/img22.460a5c88.jpg"
+  },
+  {
+    "revision": "445baef6707bad1136e2d5d6d3dd41d4",
+    "url": "/Qatar_Living_website/static/media/img23.445baef6.jpg"
+  },
+  {
+    "revision": "6709f1aca3d9047f36d4bb400659045c",
+    "url": "/Qatar_Living_website/static/media/img24.6709f1ac.jpg"
+  },
+  {
+    "revision": "63c6db23a0824f11f1180f614b03ad97",
+    "url": "/Qatar_Living_website/static/media/img25.63c6db23.jpg"
+  },
+  {
+    "revision": "b7ad5eb70110168436c3dc676a5b4079",
+    "url": "/Qatar_Living_website/static/media/img26.b7ad5eb7.jpg"
+  },
+  {
+    "revision": "fb37ef6f133ae049f31821e4358c1ea5",
+    "url": "/Qatar_Living_website/static/media/img27.fb37ef6f.jpg"
+  },
+  {
+    "revision": "bd966e1372128a1aadf58e951f5532dc",
+    "url": "/Qatar_Living_website/static/media/img28.bd966e13.jpg"
+  },
+  {
+    "revision": "02a7042905e5934750775b9b8bd4016c",
+    "url": "/Qatar_Living_website/static/media/img29.02a70429.jpg"
+  },
+  {
+    "revision": "a052abf44edc3ce23616cef3909e4b42",
+    "url": "/Qatar_Living_website/static/media/img31.a052abf4.jpg"
+  },
+  {
+    "revision": "26173d3e1dd24caf00c81983acc20a60",
+    "url": "/Qatar_Living_website/static/media/img32.26173d3e.jpg"
+  },
+  {
+    "revision": "b74e7ce82d432e652c5f585014b787a3",
+    "url": "/Qatar_Living_website/static/media/img34.b74e7ce8.jpeg"
+  },
+  {
+    "revision": "aa0ae3497318a3c75a2349a10c459799",
+    "url": "/Qatar_Living_website/static/media/img35.aa0ae349.jpeg"
+  },
+  {
+    "revision": "4483d4b3515bc0699b93fc962ae64166",
+    "url": "/Qatar_Living_website/static/media/img36.4483d4b3.jpg"
+  },
+  {
+    "revision": "6d455fc07c5da361c906e6e9efd6e234",
+    "url": "/Qatar_Living_website/static/media/img37.6d455fc0.jpg"
+  },
+  {
+    "revision": "007a50906606414059120f1b5a36f9cb",
+    "url": "/Qatar_Living_website/static/media/img39.007a5090.jpg"
+  },
+  {
+    "revision": "52956af97f5430cc7fc9d68db958b432",
+    "url": "/Qatar_Living_website/static/media/img40.52956af9.jpg"
+  },
+  {
+    "revision": "1b68600376a2dcd4c1cb1f78b2425faf",
+    "url": "/Qatar_Living_website/static/media/img41.1b686003.jpg"
+  },
+  {
+    "revision": "467813e760c51aacd856a1cd185981f8",
+    "url": "/Qatar_Living_website/static/media/img43.467813e7.jpg"
+  },
+  {
+    "revision": "9859c7e4eaa8dccf837690bcd39f0adf",
+    "url": "/Qatar_Living_website/static/media/img44.9859c7e4.jpg"
+  },
+  {
+    "revision": "2e18f5ffc04774477068adb7ae65b25a",
+    "url": "/Qatar_Living_website/static/media/img46.2e18f5ff.jpg"
+  },
+  {
+    "revision": "1b77689481b5dc7e5223ac157c36f099",
+    "url": "/Qatar_Living_website/static/media/img47.1b776894.png"
+  },
+  {
+    "revision": "a35962604de6a3ea960273ae555cd842",
+    "url": "/Qatar_Living_website/static/media/img48.a3596260.png"
+  },
+  {
+    "revision": "229137c66a608fa1ef19b6ecabd547dd",
+    "url": "/Qatar_Living_website/static/media/img49.229137c6.jpg"
+  },
+  {
+    "revision": "ba6281aeb5b8d2d65f906bde525e19bf",
+    "url": "/Qatar_Living_website/static/media/img50.ba6281ae.png"
+  },
+  {
+    "revision": "99cde353b5c7387e77c9863658bea18e",
+    "url": "/Qatar_Living_website/static/media/img51.99cde353.png"
+  },
+  {
+    "revision": "788679c3dbd5cb500908e0945ddce5b0",
+    "url": "/Qatar_Living_website/static/media/img52.788679c3.jpg"
+  },
+  {
+    "revision": "f588d5bdbe9443d3837cb7f125c8dbbd",
+    "url": "/Qatar_Living_website/static/media/img53.f588d5bd.jpg"
+  },
+  {
+    "revision": "bd3167c694602ee34237b484dba4055c",
+    "url": "/Qatar_Living_website/static/media/img55.bd3167c6.jpg"
+  },
+  {
+    "revision": "90d02d87e656fa9f146218750baf1d2f",
+    "url": "/Qatar_Living_website/static/media/img56.90d02d87.jpg"
+  },
+  {
+    "revision": "f047bcfe66cc1f3e317b07e15ced58e7",
+    "url": "/Qatar_Living_website/static/media/img57.f047bcfe.jpg"
+  },
+  {
+    "revision": "af030609b959e9a7cf90d4456685019a",
+    "url": "/Qatar_Living_website/static/media/img58.af030609.jpg"
+  },
+  {
+    "revision": "14695fdf4f779c08bfa5ff859b227b71",
+    "url": "/Qatar_Living_website/static/media/img59.14695fdf.png"
+  },
+  {
+    "revision": "97c558c280007b9e9f53d78667351f8b",
+    "url": "/Qatar_Living_website/static/media/img60.97c558c2.png"
+  },
+  {
+    "revision": "32d804b5de30159707aad08282cebcdc",
+    "url": "/Qatar_Living_website/static/media/img61.32d804b5.png"
+  },
+  {
+    "revision": "c920ec15560c7b31f98a847d56774527",
+    "url": "/Qatar_Living_website/static/media/img62.c920ec15.png"
+  },
+  {
+    "revision": "60827837dc5336c82e831d1a2588fd88",
+    "url": "/Qatar_Living_website/static/media/img63.60827837.jpg"
+  },
+  {
+    "revision": "0dedc0ec08fbfcb7c0cd5752740efa8e",
+    "url": "/Qatar_Living_website/static/media/img64.0dedc0ec.png"
+  },
+  {
+    "revision": "3095319a18994d8287b36479d12a334f",
+    "url": "/Qatar_Living_website/static/media/img65.3095319a.png"
+  },
+  {
+    "revision": "6f6b964c8321ce4ce42c9c247f7010b4",
+    "url": "/Qatar_Living_website/static/media/img66.6f6b964c.png"
+  },
+  {
+    "revision": "73ee3feb99f50632f50e84679a1fcc74",
+    "url": "/Qatar_Living_website/static/media/img67.73ee3feb.jpeg"
+  },
+  {
+    "revision": "c7b34427de92b4f23d9bbb73125219ce",
+    "url": "/Qatar_Living_website/static/media/img68.c7b34427.jpg"
+  },
+  {
+    "revision": "fb7dda5b7a97197d55df522da8d181ac",
+    "url": "/Qatar_Living_website/static/media/img69.fb7dda5b.jpg"
+  },
+  {
+    "revision": "64b158e917a34339cac23043877acb2a",
+    "url": "/Qatar_Living_website/static/media/img70.64b158e9.jpg"
+  },
+  {
+    "revision": "0ec4b2c1f95bffddbeca04540643fa36",
+    "url": "/Qatar_Living_website/static/media/img72.0ec4b2c1.jpg"
+  },
+  {
+    "revision": "1936b69317022798634b4142c493b4c1",
+    "url": "/Qatar_Living_website/static/media/img73.1936b693.jpg"
+  },
+  {
+    "revision": "b3429efe2ce01d118ae1284bee7d0798",
+    "url": "/Qatar_Living_website/static/media/img74.b3429efe.jpg"
+  },
+  {
+    "revision": "e366b92aa0b0b1470312239183c9bc15",
+    "url": "/Qatar_Living_website/static/media/img75.e366b92a.jpg"
+  },
+  {
+    "revision": "d31502c06988cd9935598f6add156658",
+    "url": "/Qatar_Living_website/static/media/img76.d31502c0.png"
+  },
+  {
+    "revision": "cbea69b33852f30d3742cf1444eba559",
+    "url": "/Qatar_Living_website/static/media/img77.cbea69b3.jpeg"
+  },
+  {
+    "revision": "0a56cf560600051028ab5346f09f779e",
+    "url": "/Qatar_Living_website/static/media/img78.0a56cf56.png"
+  },
+  {
+    "revision": "7091d673ea96abacdb97ab7e44142d61",
+    "url": "/Qatar_Living_website/static/media/img79.7091d673.png"
+  },
+  {
+    "revision": "a8df17e70777cd8af84499de285344fb",
+    "url": "/Qatar_Living_website/static/media/img80.a8df17e7.png"
+  },
+  {
+    "revision": "097424a75253a5b288a3989aaca8102e",
+    "url": "/Qatar_Living_website/static/media/img81.097424a7.png"
+  },
+  {
+    "revision": "ba6281aeb5b8d2d65f906bde525e19bf",
+    "url": "/Qatar_Living_website/static/media/img82.ba6281ae.png"
+  },
+  {
+    "revision": "7713f79b5e3094e9804bfd815379b578",
+    "url": "/Qatar_Living_website/static/media/img84.7713f79b.png"
+  },
+  {
+    "revision": "6c99f09c7efe16c495f80549dd20b23a",
+    "url": "/Qatar_Living_website/static/media/img85.6c99f09c.jpg"
+  },
+  {
+    "revision": "17fba9b74742968ec213396895312f46",
+    "url": "/Qatar_Living_website/static/media/img86.17fba9b7.jpg"
+  },
+  {
+    "revision": "bd9a8b2aac32749521535833ffd4d7e0",
+    "url": "/Qatar_Living_website/static/media/img87.bd9a8b2a.jpg"
+  },
+  {
+    "revision": "286ad2eb3f77d341abc040e3a90a7908",
+    "url": "/Qatar_Living_website/static/media/img89.286ad2eb.jpeg"
+  },
+  {
+    "revision": "92af543c2b5d46aef65f48ef0fea08a4",
+    "url": "/Qatar_Living_website/static/media/img90.92af543c.jpg"
+  },
+  {
+    "revision": "1858bac829e7730103150bf55b555bd4",
+    "url": "/Qatar_Living_website/static/media/img91.1858bac8.jpg"
+  },
+  {
+    "revision": "ba6f3280558177711e32a64291bf85eb",
+    "url": "/Qatar_Living_website/static/media/img92.ba6f3280.jpg"
+  },
+  {
+    "revision": "dfc8d2054238888ac13a3eda79123d6d",
+    "url": "/Qatar_Living_website/static/media/img93.dfc8d205.jpg"
+  },
+  {
+    "revision": "3357c69cb277cf9e1a79938a74a75f00",
+    "url": "/Qatar_Living_website/static/media/img94.3357c69c.jpeg"
+  },
+  {
+    "revision": "210048a35c740f6fd8293e5dd3b0a211",
+    "url": "/Qatar_Living_website/static/media/img95.210048a3.jpg"
+  },
+  {
+    "revision": "33c08210ce938b0cf3c1ae6ecd708b1e",
+    "url": "/Qatar_Living_website/static/media/img96.33c08210.jpg"
+  },
+  {
+    "revision": "df2d50265530f9e4c94142f8ad661e40",
+    "url": "/Qatar_Living_website/static/media/img97.df2d5026.jpg"
+  },
+  {
+    "revision": "a1ba3d3cf7991ca2d7ae8bb7255ada2b",
+    "url": "/Qatar_Living_website/static/media/img99.a1ba3d3c.jpg"
+  },
+  {
+    "revision": "d454170ffbdc737087385db5652ee610",
+    "url": "/Qatar_Living_website/static/media/img100.d454170f.jpg"
+  },
+  {
+    "revision": "2c67ccaf3c1143692bfabaad69ca9cc9",
+    "url": "/Qatar_Living_website/static/media/img101.2c67ccaf.jpg"
+  },
+  {
+    "revision": "5d1b818e287e941c850fb900c2c507c2",
+    "url": "/Qatar_Living_website/static/media/shakehand.5d1b818e.PNG"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Qatar_Living_website/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "0b1a8e1d5d603c09ef1cadbe7c277664",
+    "url": "/Qatar_Living_website/index.html"
+  }
+];
